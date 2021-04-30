d6e874d48da745fe60b5f8717ce85006e87530e5
h1pd34d <hipdead010@outlook.com>
2021-04-30 00:37:57 -0500

    se agregan patrones de diseño, se corrigen sugerencias por parte del profesor y se agregan 3 nuevas clases (Menu, recibirInformacion y ColorFactory)

--- src/com/company/factory/ColorFactory.java
+++ src/com/company/factory/ColorFactory.java
@@ -11,24 +11,25 @@ public class ColorFactory {
 
     public ColorFactory(Color color) {
 
+        do {
+            System.out.println("Seleccione el color para el vehiculo");
+            System.out.println("[1]NEGRO - [2]BLANCO - [3]AZUL - [4]VERDE - [5]GRIS");
+            seleccion_c = color_seleccionado.nextInt();
 
-        System.out.println("Seleccione el color para el vehiculo");
-        System.out.println("[1]NEGRO - [2]BLANCO - [3]AZUL - [4]VERDE - [5]GRIS");
-        seleccion_c = color_seleccionado.nextInt();
-
-        if (seleccion_c == 1) {
-            this.color = Color.NEGRO;
-        } else if (seleccion_c == 2) {
-            this.color = Color.BLANCO;
-        } else if (seleccion_c == 3) {
-            this.color = Color.AZUL;
-        } else if (seleccion_c == 4) {
-            this.color = Color.VERDE;
-        } else if (seleccion_c == 5) {
-            this.color = Color.GRIS;
-        } else {
-            System.out.println("Seleccion Invalida");
-        }
+            if (seleccion_c == 1) {
+                this.color = Color.NEGRO;
+            } else if (seleccion_c == 2) {
+                this.color = Color.BLANCO;
+            } else if (seleccion_c == 3) {
+                this.color = Color.AZUL;
+            } else if (seleccion_c == 4) {
+                this.color = Color.VERDE;
+            } else if (seleccion_c == 5) {
+                this.color = Color.GRIS;
+            } else {
+                System.out.println("Seleccion incorrecta vuelva a escribir de 1 a 5");
+            }
+        }while(seleccion_c > 5);
     }
 
 
